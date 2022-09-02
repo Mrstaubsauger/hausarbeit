2043ce2e622aab9d4b680ce080294df703384a6f
Stefan Eske (Arbeit) <Stefan Eske (Arbeit)@DESKTOP-1VDGJ84.speedport.ip>
2022-09-02 23:49:56 +0200

    test

--- ProtokollManagerView/src/main/java/ProtokollManagerView/StefanEske/Spring/ProtokollController.java
+++ ProtokollManagerView/src/main/java/ProtokollManagerView/StefanEske/Spring/ProtokollController.java
@@ -1,5 +1,5 @@
 package ProtokollManagerView.StefanEske.Spring;
-
+//GITHUB TEST
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
